77efc2fd8bd484acba73650bf8df5789b8779221
Keegan Quinn <keegan@quinn.tk>
2020-01-05 12:45:16 -0800

    Include Bootstrap CSS and JS in example page.

--- src/example.js
+++ src/example.js
@@ -1,5 +1,8 @@
 import { MusicControl } from './index';
 
+require('bootstrap/dist/css/bootstrap.css');
+require('bootstrap/dist/js/bootstrap.js');
+
 const musicControl = new MusicControl();
 musicControl.start();
 musicControl.load();
