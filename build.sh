96d16640f545665101a742427ae51075f0ceba28
Keegan Quinn <keegan@qtk.io>
2022-08-02 16:56:17 -0700

    Switch to TypeDoc for documentation.

--- build.sh
+++ build.sh
@@ -11,8 +11,7 @@ npm -s run clean
 npm -s run prepack
 
 npm -s test || true
-npm -s run jsdoc || true
-npm -s run pkglint || true
+npm -s run doc || true
 
 npm -s run lint -- --format node_modules/eslint-formatter-checkstyle-* \
     > checkstyle-eslint.xml || true
